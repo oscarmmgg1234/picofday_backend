0ca74261545a26843309d66ec7caea665262840a
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2021-12-23 20:52:44 -0800

    Update server_main.js

--- server_main.js
+++ server_main.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const { listen_port } = require('./utils');
 const {Server} = require('./server')
-var bodyParser = require('body-parser');
+
 
 //app and api init
 const server = new Server()
 const api = express();
 
-//api init config
-api.use(bodyParser);
-api.use(bodyParser.json());
-api.use(bodyParser.urlencoded({extended: true}));
-
-//server.connect();
+//app and api config
+server.connect();
 api.listen(listen_port, ()=>{console.log("server init succeeded: listening on port: " + listen_port);});
 
 // app
-//server.run()
+server.run()
 
 api.post('/uploadImage', (req, res)=>{
     DB.uploadImage(req.body);
@@ -24,9 +20,8 @@ api.post('/uploadImage', (req, res)=>{
 })
 
 api.get('/fetchImage', (req, res)=>{
-    //response.setHeader("ContentType", "application/json");
-    //server.getPickOfDayObj((image)=>response.send(image)); 
-    res.send("oscar")
+    response.setHeader("ContentType", "application/json");
+    server.getPickOfDayObj((image)=>response.send(image)); 
 })
 
 
