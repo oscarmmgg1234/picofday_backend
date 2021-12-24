5c451b318d643624de439668c7cc7857817e44e2
oscar maldonado-garcia <45194139+oscarmmgg1234@users.noreply.github.com>
2021-12-23 20:16:37 -0800

    Update server_main.js

--- server_main.js
+++ server_main.js
@@ -2,31 +2,30 @@ const express = require('express');
 const { listen_port } = require('./utils');
 const {Server} = require('./server')
 var bodyParser = require('body-parser');
-const res = require('express/lib/response');
 
+//app and api init
 const server = new Server()
 const api = express();
 
-api.use(bodyParser)
-api.use(bodyParser.json())
+//api init config
+api.use(bodyParser);
+api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({extended: true}));
 
-//server and api init
 server.connect();
 api.listen(listen_port, ()=>{});
-
 
 // app
 server.run()
 
 api.post("/uploadImage", (request, response)=>{
-    DB.uploadImage(request.body)
-    response(response.status)
+    DB.uploadImage(request.body);
+    response(response.status);
 })
 
 api.get("/fetchImage", (request, response)=>{
     response.setHeader("ContentType", "application/json");
-    server.getPickOfDayObj((image)=>response.send(image)) 
+    server.getPickOfDayObj((image)=>response.send(image)); 
 })
 
 
